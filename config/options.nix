c9d2de92f718c9acb67271437dc9ea21879821b4
zinc <zinc97neo@proton.me>
2024-06-08 19:55:22 +0800

    feat(global): init ZincVim

---
+++ config/options.nix
@@ -0,0 +1,28 @@
+{ ... }: {
+  config = {
+    # Options.
+    opts = {
+      # Indent settings.
+      tabstop = 2;
+      shiftwidth = 2;
+      expandtab = true;
+      # nowrap
+      wrap = false;
+      # Line number.
+      number = true;
+      relativenumber = true;
+      signcolumn = "yes";
+      cursorline = true;
+      splitright = true;
+      splitbelow = true;
+      # Search case sensitivity.
+      ic = true;
+    };
+
+    # Clipboard.
+    clipboard = {
+      register = "unnamedplus";
+      providers.xclip.enable = true;
+    };
+  };
+}
