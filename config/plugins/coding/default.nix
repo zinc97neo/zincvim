e1149138b787629c60209a9290acd738fd78bf85
zinc <zinc97neo@proton.me>
2024-06-18 22:45:05 +0800

    feat(coding): install neogen nvim-autopairs comment

--- config/plugins/coding/default.nix
+++ config/plugins/coding/default.nix
@@ -2,4 +2,21 @@
   imports = [
     ./neoformat.nix
   ];
+  plugins = {
+    neogen = {
+      enable = true;
+    };
+    nvim-autopairs = {
+      enable = true;
+    };
+    comment = {
+      enable = true;
+      settings = {
+        toggler = {
+          block = "gbc";
+          line = "gcc";
+        };
+      };
+    };
+  };
 }
